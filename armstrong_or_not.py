b9776888e698ab48cea28bbb40038c97d8681692
Birajit Saikia <birajitsaikia@Birajits-MacBook-Pro.local>
2024-10-15 21:43:47 +0530

    code to find the no of diagonals of a polygon

---
+++ armstrong_or_not.py
@@ -0,0 +1,16 @@
+def is_armstrong(n):
+    num_digits = 0
+    number = n
+
+    while number > 0:
+        num_digits += 1
+        number //= 10
+    
+    armstrong_sum = 0
+    number = n 
+    while number > 0:
+        digit = number % 10
+        armstrong_sum += digit ** num_digits
+        number //= 10
+    
+    return armstrong_sum == n

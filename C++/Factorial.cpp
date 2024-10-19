89653023fb3ff62ee26d4327272f512b1de0c0d1
Sattwik-8 <sattwikdhara70@gmail.com>
2024-10-19 10:19:05 +0530

    Update

--- C++/Factorial.cpp
+++ C++/Factorial.cpp
@@ -1,24 +1,25 @@
 #include<iostream>
 using namespace std;
 
-
-int main(){
+int main() {
 
     int num;
-cout<<"Enter the number : ";
-cin>>num;
-int fact=1;
+    cout << "Enter a non-negative number: ";
+    cin >> num;
 
-for(int i = num ; i>=1 ; i--){
-fact=fact*i;
-}
+    // Ensure the number is non-negative
+    if (num < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        int fact = 1;
 
+        // Factorial of 0 is 1
+        for (int i = num; i >= 1; i--) {
+            fact = fact * i;
+        }
 
-cout<<"The factorial of  "<<num<< " is " << fact<<endl;
+        cout << "The factorial of " << num << " is " << fact << endl;
+    }
 
-
-
-    return 0 ;
-\
-
+    return 0;
 }

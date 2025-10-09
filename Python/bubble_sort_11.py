36b855128fbf16945fed864529ddaa7068df2404
Pallab Hazra <hazrap404@gmail.com>
2025-10-09 11:04:17 +0530

    Add new Code

--- Python/bubble_sort_11.py
+++ Python/bubble_sort_11.py
@@ -4,13 +4,11 @@ def bubble_sort(arr):
     n = len(arr)
     # Traverse through all array elements
     for i in range(n - 1):
-        # Last i elements are already in place
         for j in range(0, n - i - 1):
             # Swap if the element found is greater than the next element
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-# Example usage
 numbers = [64, 34, 25, 12, 22, 11, 90]
 print("Unsorted list:", numbers)
 

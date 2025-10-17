7dc0747f8d65e5273cdb15890c8b6a8e3c4a681a
ypeee1208-maker <ypeee1208@gmail.com>
2025-10-17 19:26:49 +0530

    Added Counting Sort

---
+++ C++ Code/countingSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void countingSort(vector<int>& arr) {
+    int maxVal = *max_element(arr.begin(), arr.end());
+    int minVal = *min_element(arr.begin(), arr.end());
+    int range = maxVal - minVal + 1;
+
+    vector<int> count(range, 0), output(arr.size());
+    for (int num : arr) count[num - minVal]++;
+    for (int i = 1; i < range; i++) count[i] += count[i - 1];
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        output[count[arr[i] - minVal] - 1] = arr[i];
+        count[arr[i] - minVal]--;
+    }
+    arr = output;
+}
+
+int main() {
+    vector<int> arr = {4, 2, 2, 8, 3, 3, 1};
+    countingSort(arr);
+    for (int num : arr) cout << num << " ";
+    return 0;
+}

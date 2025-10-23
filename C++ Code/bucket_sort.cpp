5d28838e20b541e4e69fc42bee11526022cdc226
star-arpit12 <asharma1208@outlook.com>
2025-10-23 18:33:52 +0530

    Added bucket sort

---
+++ C++ Code/bucket_sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void bucketSort(float arr[], int n) {
+    vector<float> buckets[n];
+
+    
+    for (int i = 0; i < n; i++) {
+        int idx = n * arr[i]; 
+        buckets[idx].push_back(arr[i]);
+    }
+
+
+    for (int i = 0; i < n; i++)
+        sort(buckets[i].begin(), buckets[i].end());
+
+    
+    int index = 0;
+    for (int i = 0; i < n; i++)
+        for (float val : buckets[i])
+            arr[index++] = val;
+}
+
+int main() {
+    float arr[] = {0.42, 0.32, 0.23, 0.52, 0.25, 0.47, 0.51};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bucketSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
